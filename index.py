bd5469e8e441b5df81f1a0d9a908b8f90c5c14bb
shankar <shankarramavat3064@gmail.com>
2025-11-25 14:35:02 +0530

    Fix syntax error in index.py

--- index.py
+++ index.py
@@ -22,30 +22,6 @@ try:
     # Ensure secret key is set
     application.secret_key = os.getenv('FLASK_SECRET_KEY', 'fixed_secret_key_for_vercel')
 
-import sys
-import os
-import traceback
-
-# Add the trio directory to the path so we can import app.py
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'trio'))
-
-try:
-    # Import the main Flask app from trio/app.py
-    from app import app as application
-
-    # -------------------------------------------------
-    # INJECT HARDCODED DATABASE CREDENTIALS
-    # -------------------------------------------------
-    # This forces the app to use the correct Railway DB, ignoring missing env vars
-    application.config['MYSQL_HOST'] = 'interchange.proxy.rlwy.net'
-    application.config['MYSQL_USER'] = 'root'
-    application.config['MYSQL_PASSWORD'] = 'EmoLxiyvQALpwpAwrniXpYeJhpIhjavo'
-    application.config['MYSQL_DB'] = 'railway'
-    application.config['MYSQL_PORT'] = 48465
-    
-    # Ensure secret key is set
-    application.secret_key = os.getenv('FLASK_SECRET_KEY', 'fixed_secret_key_for_vercel')
-
     # Vercel expects 'app' or 'handler'
     app = application
     handler = application
